012ebe316b036142e77235579231a339e1d9552a
Basavaraj <bnarasapur@gmail.com>
2021-06-28 16:18:25 +0530

    init commit ctr+c is working

---
+++ catkin_ws/src/my_pkg/src/main.cpp
@@ -0,0 +1,43 @@
+#include <sstream>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "std_msgs/Float64.h"
+#include "stack.h"
+#include <signal.h>
+
+Basavaraj process;
+
+volatile sig_atomic_t stop;
+
+void inthand(int signum) {
+    process.ProcessTeardown();
+    stop = 1;
+    exit(signum);
+}
+
+
+int main(int argc, char **argv)
+{
+  signal(SIGINT, inthand);
+  process.Initialize();
+  process.PublisherSetup();
+  process.SubscriberSetup();
+
+
+  ros::Rate loop_rate(10);
+
+
+  while (ros::ok())
+  {
+
+    process.CallSuscribers();
+    process.CallPublisher();
+    loop_rate.sleep();
+    ros::spinOnce();
+
+
+  }
+
+  
+  return EXIT_SUCCESS;
+}
---
+++ catkin_ws/src/my_pkg/src/stack_impl.cpp
@@ -0,0 +1,84 @@
+#include "stack.h"
+#define SUCCESS_RETURN_CODE 0
+#define ERROR_RETURN_CODE -1
+
+#define ROS_HISTORY_DEPTH 10
+
+Basavaraj::Basavaraj(){}
+
+int32_t Basavaraj::Initialize(){
+  printf("Initialize()\n");
+  fflush(stdout);
+
+  // Start roscore in our container (must be done before ros::init())
+  roscore_process_.reset(new subprocess::Popen("roscore"));
+
+  // Initialize ROS
+  int argc = 0;
+  char **argv = NULL;
+  ros::init(argc, argv, "stack", ros::init_options::NoSigintHandler);
+  node_.reset(new ros::NodeHandle());
+  spinner_.reset(new ros::AsyncSpinner(4));
+  spinner_->start();
+
+
+  return SUCCESS_RETURN_CODE;
+}
+
+
+
+int32_t Basavaraj::PublisherSetup(){
+	    stack_version_publisher_ = node_->advertise<std_msgs::String>(
+        kStackVersionTopic, ROS_HISTORY_DEPTH);
+	return SUCCESS_RETURN_CODE;
+}
+
+int32_t Basavaraj::SubscriberSetup(){
+	
+
+	return SUCCESS_RETURN_CODE;
+}
+
+int32_t Basavaraj::CallSuscribers(){
+
+
+	return SUCCESS_RETURN_CODE;
+}
+
+int32_t Basavaraj::CallPublisher(){
+
+
+	return SUCCESS_RETURN_CODE;
+}
+
+int32_t Basavaraj::KillPublisher(){
+
+	return SUCCESS_RETURN_CODE;
+}
+
+
+int32_t SetRate(){
+
+	return SUCCESS_RETURN_CODE;
+}
+
+
+
+int32_t Basavaraj::ProcessTeardown() {
+  printf("Process killed..()\n");
+  fflush(stdout);
+
+  spinner_->stop();
+  ros::shutdown();
+
+  // Stop roscore using SIGINT (Ctrl-C)
+  roscore_process_->kill(2);
+
+  return SUCCESS_RETURN_CODE;
+}
+
+
+
+
+
+
